86ea08626f7ecba5ffb65ed1ad027bd16d58056b
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-09-13 10:03:02 +0700

    Updates

--- static/js/main.2bcddca6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcart=this.webpackJsonpcart||[]).push([[0],{4:function(e,t,c){"use strict";c.r(t);c(0);var n=c(2),r=c.n(n),s=(c(9),c(3)),a=Object(s.jsx)("h1",{children:"Hello React"});r.a.render(a,document.getElementById("root"))}},[[4,1,2]]]);
-//# sourceMappingURL=main.2bcddca6.chunk.js.map
