3aec8b30fc492be3d39510a9353f6f7329a898cc
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-09-13 10:01:48 +0700

    2.Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import reactDom from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/counter'
 
-const element = <h1>Hello React</h1>
+const App = () => {
+  return <Counter />
+}
 
-reactDom.render(element, document.getElementById('root'))
+reactDom.render(<App />, document.getElementById('root'))
